01059b29963108ad6e419c3a54bcec64aaf3f8e5
João Pedro <joao.medeiros@idus.com.br>
2024-09-19 12:26:09 -0300

    criando metodo post e get usuarios

--- src/usuario.controller.ts
+++ src/usuario.controller.ts
@@ -1,11 +1,20 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { UsuarioRepository } from "./usuario.repository";
 
 @Controller('/usuarios')
 export class UsuarioController {
 
+    private usuarioRepository = new UsuarioRepository
+
     @Post()
     async criaUsuario(@Body() dadosDoUsuarios) {
+        this.usuarioRepository.salvar(dadosDoUsuarios);
         return dadosDoUsuarios
+    }
+
+    @Get()
+    async listUsuarios() {
+        return this.usuarioRepository.listar();
     }
 
 }
--- src/usuario.repository.ts
+++ src/usuario.repository.ts
@@ -1,3 +1,13 @@
 export class UsuarioRepository {
-    
+    private usuarios = [];
+
+    async salvar(usuarios) {
+        this.usuarios.push(usuarios);
+    }
+
+    async listar() {
+        return this.usuarios;
+    }
 }
+
+//2 capitulo ep 5
